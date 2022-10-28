ef7f0ad267e05be625b73364fb96dd1f305b84eb
salimaNibareke <nibarekesalima@gmail.com>
2022-10-28 12:21:21 +0200

    shopping store

---
+++ src/redux/index.js
@@ -0,0 +1,18 @@
+// For Add Item to Cart
+export const addCart = (product) =>{
+    return{
+        type:"ADDITEM",
+        payload : product
+    }
+
+    
+}
+// For Add Item to Cart
+export const addCart = (product) =>{
+    return{
+        type:"ADDITEM",
+        payload : product
+    }
+
+    
+}
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,11 @@
+import handlecart from "./handle";
+import {combineReducers} from './redux'
+
+const rootReducers = combineReducers({
+    handlecart,
+
+}
+    
+)
+export default rootReducers;
+
